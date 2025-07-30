0229c9ddf9c029651ebaaba027929b45db1925cc
shahids444 <shahidshaiks444@gmail.com>
2025-07-31 00:06:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function DataFetcher() {
   const [data, setData] = useState(null);
